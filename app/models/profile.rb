d2f5e4d104cc0a5c887a6342d9c4c573671e1a53
Amr Alaa <n1amr1@gmail.com>
2015-12-05 01:02:49 +0200

    11. AddvalidationtoUserandProfilemodels

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,20 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :one_null
+  def one_null
+    if(first_name.nil? and last_name.nil?)
+      errors.add(:last_name, "and First name cannot be both empty")
+    end
+  end
+
+  validates :gender, inclusion: ["male", "female"]
+
+  validate :no_male_sue
+  def no_male_sue
+    if(first_name == "Sue" and gender == "male")
+      errors.add(:first_name, "cannot be male")
+    end
+  end
+
 end
