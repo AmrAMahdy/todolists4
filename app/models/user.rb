e5058663207bc8b0d6527b599a51a494855fea87
Amr Alaa <n1amr1@gmail.com>
2015-12-05 01:06:55 +0200

    13.  Add  a method  to  the User  model class called  get_completed_count,  which:

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   has_many :todo_items, through: :todo_lists, source: :todo_items
 
   validates :username, presence: true
+
+  def get_completed_count
+    c = 0
+    todo_items.each do |i|
+      if(i.completed)
+        c += 1
+      end
+    end
+    return c
+  end
 end
